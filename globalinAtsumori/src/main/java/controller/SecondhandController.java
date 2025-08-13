ec7c0a52e4bf1f651f7df8c8ffd5d1bfa93b6506
tillimakeit <gooday0102@naver.com>
2025-08-13 18:32:34 +0900

    sd 컨트롤러 수정

--- globalinAtsumori/src/main/java/controller/SecondhandController.java
+++ globalinAtsumori/src/main/java/controller/SecondhandController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-public class SecondhandContrller {
+public class SecondhandController {
 	
 	@GetMapping("/secondhandMain")
 	public String shMainPage() {
