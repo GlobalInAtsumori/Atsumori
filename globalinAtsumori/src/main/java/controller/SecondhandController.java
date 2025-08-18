17aedf6c2d5e695391dd106e9223fb2949a55ba6
tillimakeit <gooday0102@naver.com>
2025-08-18 19:43:27 +0900

    sh controller 글,이미지 db 저장

--- globalinAtsumori/src/main/java/controller/SecondhandController.java
+++ globalinAtsumori/src/main/java/controller/SecondhandController.java
@@ -7,14 +7,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import dao.SecondhandDAO;
 import domain.SecondhandImageVO;
 import domain.SecondhandVO;
 import lombok.RequiredArgsConstructor;
+import service.S3Service;
 
 @Controller
 public class SecondhandController {
+	
+	private final S3Service s3Service;
+	public SecondhandController(S3Service s3service) {
+		this.s3Service = s3service;
+	}
 	
 	@GetMapping("/secondhandMain")
 	public String shMainPage() {
@@ -28,18 +35,28 @@ public class SecondhandController {
     }
 	
 	@PostMapping("/secondhand/write")
-	public String writeSecondhandArticle(SecondhandVO shArticle, @RequestParam("imageUrl") String imageUrl) {
-		// 로그인 기능 없어서 임시로 '김철수'로 하드코딩
-        shArticle.setMemberNo(4);
-        shArticle.setCreateDate(new Timestamp(System.currentTimeMillis()));
-        
-        //img VO 생성
-        SecondhandImageVO imageVO = new SecondhandImageVO();
-        imageVO.setTradeImgUrl(imageUrl);
-        
-        // DAO 호출
-        SecondhandDAO dao = SecondhandDAO.getInstance();
-        dao.insertSHArticle(shArticle, imageVO);
+	public String writeSecondhandArticle(SecondhandVO shArticle, @RequestParam("imageFile") MultipartFile file) {
+		
+		try {
+			// 로그인 기능 없어서 임시로 '김철수'로 하드코딩
+			shArticle.setMemberNo(4);
+			shArticle.setCreateDate(new Timestamp(System.currentTimeMillis()));
+			
+			//img 파일을 S3에 업로드하여 URL 받기
+			String imageUrl = s3Service.uploadFile(file);
+			
+			//img VO 생성
+			SecondhandImageVO imageVO = new SecondhandImageVO();
+			imageVO.setTradeImgUrl(imageUrl);
+			
+			// 글과 이미지 DB에 저장
+			SecondhandDAO dao = SecondhandDAO.getInstance();
+			dao.insertSHArticle(shArticle, imageVO);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
         
         return "redirect:/secondhandMain";
 	}
