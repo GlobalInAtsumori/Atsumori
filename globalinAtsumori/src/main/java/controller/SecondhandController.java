64d0f592d907282870a53c3e7f470bf0f5a01ec2
tillimakeit <gooday0102@naver.com>
2025-08-18 19:21:58 +0900

    properties 내용 수정

--- globalinAtsumori/src/main/java/controller/SecondhandController.java
+++ globalinAtsumori/src/main/java/controller/SecondhandController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import dao.SecondhandDAO;
+import domain.SecondhandImageVO;
 import domain.SecondhandVO;
 import lombok.RequiredArgsConstructor;
 
@@ -26,14 +28,18 @@ public class SecondhandController {
     }
 	
 	@PostMapping("/secondhand/write")
-	public String writeSecondhandArticle(SecondhandVO shArticle) {
+	public String writeSecondhandArticle(SecondhandVO shArticle, @RequestParam("imageUrl") String imageUrl) {
 		// 로그인 기능 없어서 임시로 '김철수'로 하드코딩
         shArticle.setMemberNo(4);
         shArticle.setCreateDate(new Timestamp(System.currentTimeMillis()));
-
-        // DAO를 통해 저장
+        
+        //img VO 생성
+        SecondhandImageVO imageVO = new SecondhandImageVO();
+        imageVO.setTradeImgUrl(imageUrl);
+        
+        // DAO 호출
         SecondhandDAO dao = SecondhandDAO.getInstance();
-        dao.insertSHArticle(shArticle);
+        dao.insertSHArticle(shArticle, imageVO);
         
         return "redirect:/secondhandMain";
 	}
