e2b1e1e53052e58fa82129fdc3bf5423150ce041
tillimakeit <gooday0102@naver.com>
2025-08-18 18:33:11 +0900

    sh image VO

---
+++ globalinAtsumori/src/main/java/domain/SecondhandImageVO.java
@@ -0,0 +1,13 @@
+package domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//tradeImage 테이블
+@Getter
+@Setter
+public class SecondhandImageVO {
+	int tradeImgNo;
+	String tradeImgUrl;
+	int tradePostNo;
+}
