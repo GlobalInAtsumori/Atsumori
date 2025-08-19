91698c50d388d4db5c343d50eaaec36b59f1b8ac
tillimakeit <gooday0102@naver.com>
2025-08-19 19:31:56 +0900

    sh mapper 수정

--- globalinAtsumori/src/main/java/mapper/SecondhandMapper.java
+++ globalinAtsumori/src/main/java/mapper/SecondhandMapper.java
@@ -1,22 +1,35 @@
 package mapper;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
 import domain.SecondhandVO;
 
 @Mapper
 public interface SecondhandMapper {
-	//전체 글
-	List<SecondhandVO> selectAll();
-	
-	//글쓰기
-	void insert(SecondhandVO vo);
-	
-	//상세페이지
-	SecondhandVO selectById(int tradePostNo);
-	
-	//상태
-	void updateStatus(int tradePostNo, String status);
+	// 글 등록
+    void insertTradePost(SecondhandVO vo);
+
+    // 이미지 등록
+    void insertTradeImage(SecondhandVO vo);
+
+    // 글 수정
+    void updateTradePost(SecondhandVO vo);
+
+    // 이미지 수정
+    void updateTradeImage(SecondhandVO vo);
+
+    // 글 삭제
+    void deleteTradePost(int tradePostNo);
+
+    // 이미지 삭제
+    void deleteTradeImage(int tradePostNo);
+
+    // 단일 글 조회
+    SecondhandVO selectTradePostById(int tradePostNo);
+
+    // 메인 페이지 리스트 조회
+    List<SecondhandVO> selectTradePostList();
+
+    // 페이징용 글 개수
+    int countTradePost();
 }
