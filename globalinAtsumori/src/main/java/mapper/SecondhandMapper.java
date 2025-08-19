ac0a1953b6fe7b8ab97d9f3ac1070d67f7f8cd9b
tillimakeit <gooday0102@naver.com>
2025-08-19 19:09:51 +0900

    sh mapper interface

---
+++ globalinAtsumori/src/main/java/mapper/SecondhandMapper.java
@@ -0,0 +1,22 @@
+package mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import domain.SecondhandVO;
+
+@Mapper
+public interface SecondhandMapper {
+	//전체 글
+	List<SecondhandVO> selectAll();
+	
+	//글쓰기
+	void insert(SecondhandVO vo);
+	
+	//상세페이지
+	SecondhandVO selectById(int tradePostNo);
+	
+	//글 수정
+	void updateStatus(int tradePostNo, String status);
+}
