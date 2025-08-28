2158d7c90147ecc5c3989fafc67563ffcd007970
dnwndls411@gmail.com <dnwndls411@gmail.com>
2025-08-28 17:02:20 +0900

    feat: reviewWrite 수정

--- globalinAtsumori/src/main/java/mapper/ReviewImgMapper.java
+++ globalinAtsumori/src/main/java/mapper/ReviewImgMapper.java
@@ -1,7 +1,10 @@
 package mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import domain.ReviewImageVO;
 
+@Mapper
 public interface ReviewImgMapper {
 
 	void insertReviewImg(ReviewImageVO riVO);
